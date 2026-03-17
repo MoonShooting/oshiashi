8bf8488581985f90d7cda805cfb350a5f7d08681
soulmas16-cloud <soulmas16@gmail.com>
2026-03-17 16:44:57 +0900

    feat: route item API 및 route CRUD 로직 추가

--- backend/src/main/java/project/oshiashi/oshiashi/domain/route/entity/RouteSpotEntity.java
+++ backend/src/main/java/project/oshiashi/oshiashi/domain/route/entity/RouteSpotEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import project.oshiashi.oshiashi.domain.spot.entity.SpotEntity;
 
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -16,29 +15,26 @@ import project.oshiashi.oshiashi.domain.spot.entity.SpotEntity;
 )
 public class RouteSpotEntity {
 
-    // RouteSpot PK
+
+    
+    // RouteSpotId 추가
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "route_spot_id", nullable = false, updatable = false)
     private Long routeSpotId;
 
-    // Route와 N:1 관계
-    // 하나의 Route에 여러 Spot이 포함될 수 있음
+    // optional = false 구문을 추가 했습니다
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "route_id",
-            nullable = false,
-            foreignKey = @ForeignKey(name = "FK_Routespot_Route"))
+            nullable = false,foreignKey = @ForeignKey(name = "FK_Routespot_Route"))
     private RouteEntity route;
 
-    // Spot과 N:1 관계
-    // 하나의 Spot이 여러 Route에 포함될 수 있음
+    // optional = false 구문을 추가 했습니다
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "spot_id",
-            nullable = false,
+    @JoinColumn(name = "spot_id", nullable = false,
             foreignKey = @ForeignKey(name = "FK_Routespot_Spot"))
     private SpotEntity spot;
 
-    // 루트 내 방문 순서
     @Column(name = "visit_order", nullable = false)
     private Integer visitOrder = 1;
 
@@ -57,5 +53,10 @@ public class RouteSpotEntity {
                 .spot(spot)
                 .visitOrder(visitOrder != null ? visitOrder : 1)
                 .build();
+    }
+
+    // visitOrder 수정 메서드
+    public void updateVisitOrder(Integer visitOrder) {
+        this.visitOrder = visitOrder;
     }
 }
--- backend/src/main/java/project/oshiashi/oshiashi/domain/route/repository/RouteSpotRepository.java
+++ backend/src/main/java/project/oshiashi/oshiashi/domain/route/repository/RouteSpotRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import project.oshiashi.oshiashi.domain.route.entity.RouteSpotEntity;
 import project.oshiashi.oshiashi.domain.route.entity.RouteSpotId;
 
+import java.util.Optional;
 import java.util.List;
 
 @Repository
@@ -12,6 +13,7 @@ public interface RouteSpotRepository extends JpaRepository<RouteSpotEntity, Rout
 	
 	// 특정 루트에 포함된 모든 장소(Spot)들을 순서대로 조회
 	// 리스트로 가져와서 화면에 코스를 그릴 때 사용한다고합니다
-	//List<RouteSpotEntity> findByRoute_RouteIdOrderBySequenceAsc(Long routeId);
+	List<RouteSpotEntity> findByRoute_RouteIdOrderByVisitOrderAsc(Long routeId);
 
+    Optional<RouteSpotEntity> findByRouteSpotIdAndRoute_RouteId(Long routeSpotId, Long routeId);
 }
